03070bf4ca169b84b7aee7ad2c28e2529fedd6e2
tavisca-rpawar <51902720+tavisca-rpawar@users.noreply.github.com>
2019-06-22 14:57:56 +0530

    SelectMeal Solution
    @tavisca-dagrawal Kindly review the solution

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -37,11 +38,89 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"Diet plan = [{string.Join(", ", dietPlans)}]");
             Console.WriteLine(result);
         }
-
+        public static int GetMeals(string dietPlan, int[] protein, int[] carbs, int[] fat, int[] calories)
+        {
+            int val;
+            List<int> item =new List<int>();
+            for (int i = 0; i < fat.Length; i++){
+                item.Add(i);
+            }
+            foreach(var x in dietPlan)
+            {
+                switch( x )
+                {
+                    case 'P' : 
+                        val = FIndMax(protein, item); 
+                        item = indices( protein, item, val ); break;
+                    case 'p' : 
+                        val = FIndMin(protein, item);
+                        item = indices( protein, item, val); break;
+                    case 'C' : 
+                        val = FIndMax(carbs, item);
+                        item = indices( carbs, item, val); break;
+                    case 'c' : 
+                        val = FIndMin(carbs, item);
+                        item = indices( carbs, item, val); break;
+                    case 'F' : 
+                        val = FIndMax(fat, item);
+                        item = indices( fat, item, val); break;
+                    case 'f' : 
+                        val =  FIndMin(fat, item);
+                        item = indices( fat, item, val); break;
+                    case 'T' : 
+                        val = FIndMax(calories, item);
+                        item = indices( calories, item, val); break;
+                    case 't' : 
+                        val = FIndMin(calories, item);
+                        item = indices( calories, item, val); break;
+                }
+            }
+            return item[0];
+        }
+        // To find the index of the val
+        public static List<int> indices(int[] arr, List<int> item, int val) {
+            List<int> temp = new List<int>();
+            foreach (var i in item)
+            {
+                if (arr[i] == val)
+                    temp.Add(i);
+            }
+            return temp;
+        }
+        // To find the maximum value of respective item among all
+        private static int FIndMax(int[] arr, List<int> item)
+        {
+            int max= arr[item[0]];
+            for(int i = 1 ; i < item.Count ; i++)
+            {
+                if(arr[item[i]] > max)
+                    max= arr[item[i]];
+            }          
+            return max;
+        }
+        // To find the minimum value of respective item among all
+        private static int FIndMin(int[] arr, List<int> item)
+        {
+            int min= arr[item[0]];
+            for(int i = 1 ; i < item.Count ; i++)
+            {
+                if(arr[item[i]] < min)
+                    min= arr[item[i]];
+            }
+            return min;
+        }     
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
+            //Add  your code here.
+            int[] calories = new int[protein.Length];
+            int[] result = new int[dietPlans.Length];
+            for(int i = 0; i < protein.Length; i++)
+                calories[i] = ( protein[i] + carbs[i] ) * 5 + fat[i] *9 ;
+            for(int i = 0; i < dietPlans.Length; i++)
+                result[i] = GetMeals(dietPlans[i], protein, carbs, fat, calories);
+            return result;
             throw new NotImplementedException();
-        }
+        }        
     }
 }
+
